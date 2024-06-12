49601f017f3d18617d8f790a29eadd76858c84f9
const-ae <artjom31415@googlemail.com>
2024-06-12 18:13:09 +0200

    Initial commit

---
+++ _extensions/cae-quarto-helpers/cae-quarto-helpers.lua
@@ -0,0 +1,5 @@
+return {
+  ['cae-quarto-helpers'] = function(args, kwargs, meta) 
+    return pandoc.Str("Constantin's quarto extensions!")
+  end
+}
---
+++ _extensions/cae-quarto-helpers/fixme.lua
@@ -0,0 +1,16 @@
+return {
+    ['fixme'] = function(args, kwargs, meta)
+      if quarto.doc.is_format("pdf") then
+        return pandoc.RawBlock('tex', '{\\color{red}test}')
+      elseif quarto.doc.is_format("html") then
+        return pandoc.RawBlock('html', '<span style="color:red">FIXME</span>')
+      elseif quarto.doc.is_format("docx") then
+        -- This is very hacky and does not work if the shortcode is inside a paragraph
+        -- return pandoc.RawBlock("openxml", '<w:p><w:r w:rsidRPr="005D5CCE"><w:rPr><w:color w:val="FF0000"/></w:rPr><w:t>FIXME!!</w:t></w:r></w:p>')
+        return pandoc.Emph("FIXME")
+      else
+        return pandoc.Str("FIXME")
+      end
+    end
+  }
+  
